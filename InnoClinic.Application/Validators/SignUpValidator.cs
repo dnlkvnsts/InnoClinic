a79fc509c03cd26db278b2c47a71b6db74c20c86
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-18 15:30:02 +0300

    feat(appl): add SignUpValidator

---
+++ InnoClinic.Application/Validators/SignUpValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Application.Validators
+{
+    internal class SignUpValidator
+    {
+    }
+}
