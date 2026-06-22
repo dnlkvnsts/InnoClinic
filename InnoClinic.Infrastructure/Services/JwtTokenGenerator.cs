751318c2d9311f3bf0c6a08a85f3ec2d9172dc5a
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-22 16:37:17 +0300

    feat(infra): add JwtTokenGenerator

---
+++ InnoClinic.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,65 @@
+﻿using InnoClinic.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Infrastructure.Services
+{
+    public class JwtTokenGenerator: IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public string GenerateToken(string userId, string email)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+
+            var keyStr = _configuration["JwtSettings:Key"];
+            if (string.IsNullOrEmpty(keyStr))
+                throw new Exception("JWT Key is not configured.");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyStr));
+
+
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+
+            var expiresMinutes = Convert.ToDouble(_configuration["JwtSettings:DurationInMinutes"] ?? "60");
+            var expires = DateTime.UtcNow.AddMinutes(expiresMinutes);
+
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JwtSettings:Issuer"],
+                audience: _configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
+
+
+    }
+}
