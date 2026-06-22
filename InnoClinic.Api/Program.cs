00cb15a3be73b5b0e5769e73f5012b4b969c08df
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-22 15:03:26 +0300

    feat(api): add settings in Program

--- InnoClinic.Api/Program.cs
+++ InnoClinic.Api/Program.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using InnoClinic.Application.Features.Users.Commands.SignUp;
 using InnoClinic.Application.Validators;
 using InnoClinic.Domain.Interfaces;
 using InnoClinic.Infrastructure.Persistence;
@@ -39,6 +40,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(SignUpCommand).Assembly);
+});
 
 var app = builder.Build();
 
