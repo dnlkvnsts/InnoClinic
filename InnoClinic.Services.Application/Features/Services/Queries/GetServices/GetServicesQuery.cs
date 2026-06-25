01846034fa26273cc546492bddac9b77624b8ceb
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-25 15:11:56 +0300

    feat(serv.appl): add GetServicesQueryHandler

---
+++ InnoClinic.Services.Application/Features/Services/Queries/GetServices/GetServicesQuery.cs
@@ -0,0 +1,14 @@
+﻿using InnoClinic.Services.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Services.Application.Features.Services.Queries.GetServices
+{
+  
+        public record GetServicesQuery() : IRequest<AllServicesDto>;
+   
+}
