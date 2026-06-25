2b3ee895f5913998099abd8bc4312df16bb39609
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-25 16:40:04 +0300

    feat(serv.infra): add data to database

--- InnoClinic.Services.Infrastructure/Migrations/20260625133704_SeedServicesData.Designer.cs
+++ InnoClinic.Services.Infrastructure/Migrations/20260625133704_SeedServicesData.Designer.cs
@@ -4,6 +4,7 @@ using InnoClinic.Services.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InnoClinic.Services.Infrastructure.Migrations
 {
     [DbContext(typeof(ServicesDbContext))]
-    partial class ServicesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260625133704_SeedServicesData")]
+    partial class SeedServicesData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
