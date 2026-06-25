cda8b41a3794fb649ef3a38af9b85d74c70619da
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-25 16:07:50 +0300

    feat(serv.api): add ServiceController

---
+++ InnoClinic.Services.Api/Controllers/ServiceController.cs
@@ -0,0 +1,32 @@
+﻿using InnoClinic.Services.Application.Features.Services.Queries.GetServices;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InnoClinic.Services.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServiceController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ServiceController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetDoctors(CancellationToken cancellationToken)
+        {
+
+            var query = new GetServicesQuery();
+            var services = await _mediator.Send(query, cancellationToken);
+            return Ok(services);
+        }
+
+
+
+    }
+
+}
