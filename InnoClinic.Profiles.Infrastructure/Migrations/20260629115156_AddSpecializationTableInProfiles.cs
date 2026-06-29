da5343695969410c6c3aeae3a9051afda7e924fe
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-29 15:55:25 +0300

    faet(prof.infra): add new doctor table in db

--- InnoClinic.Profiles.Infrastructure/Migrations/20260629115156_AddSpecializationTableInProfiles.cs
+++ InnoClinic.Profiles.Infrastructure/Migrations/20260629115156_AddSpecializationTableInProfiles.cs
@@ -11,6 +11,35 @@ namespace InnoClinic.Profiles.Infrastructure.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "Specialization",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AccountId",
+                table: "Doctors",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Doctors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SpecializationId",
+                table: "Doctors",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
             migrationBuilder.CreateTable(
                 name: "Specializations",
                 columns: table => new
@@ -23,85 +52,6 @@ namespace InnoClinic.Profiles.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_Specializations", x => x.Id);
                 });
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "SpecializationId",
-                table: "Doctors",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "AccountId",
-                table: "Doctors",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "DateOfBirth",
-                table: "Doctors",
-                type: "datetime2",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.Sql(@"
-        INSERT INTO [Specializations] ([Id], [SpecializationName], [IsActive])
-        SELECT NEWID(), [Specialization], 1
-        FROM [Doctors]
-        WHERE [Specialization] IS NOT NULL AND [Specialization] <> ''
-        GROUP BY [Specialization];
-    ");
-
-            migrationBuilder.Sql(@"
-        UPDATE d
-        SET d.[SpecializationId] = s.[Id]
-        FROM [Doctors] d
-        INNER JOIN [Specializations] s ON d.[Specialization] = s.[SpecializationName];
-    ");
-
-            migrationBuilder.Sql(@"
-        UPDATE [Doctors]
-        SET [AccountId] = TRY_CAST([UserId] AS uniqueidentifier)
-        WHERE [UserId] IS NOT NULL;
-    ");
-
-            migrationBuilder.Sql(@"
-        IF EXISTS (SELECT 1 FROM [Doctors] WHERE [SpecializationId] IS NULL)
-        BEGIN
-            DECLARE @DefaultSpecId UNIQUEIDENTIFIER = '00000000-0000-0000-0000-000000000001';
-            
-            IF NOT EXISTS (SELECT 1 FROM [Specializations] WHERE [Id] = @DefaultSpecId)
-            BEGIN
-                INSERT INTO [Specializations] ([Id], [SpecializationName], [IsActive])
-                VALUES (@DefaultSpecId, 'Default', 1);
-            END
-
-            UPDATE [Doctors]
-            SET [SpecializationId] = @DefaultSpecId
-            WHERE [SpecializationId] IS NULL;
-        END
-
-        UPDATE [Doctors]
-        SET [AccountId] = '00000000-0000-0000-0000-000000000000'
-        WHERE [AccountId] IS NULL;
-    ");
-
-            migrationBuilder.AlterColumn<Guid>(
-                name: "SpecializationId",
-                table: "Doctors",
-                type: "uniqueidentifier",
-                nullable: false,
-                oldClrType: typeof(Guid),
-                oldType: "uniqueidentifier",
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<Guid>(
-                name: "AccountId",
-                table: "Doctors",
-                type: "uniqueidentifier",
-                nullable: false,
-                oldClrType: typeof(Guid),
-                oldType: "uniqueidentifier",
-                oldNullable: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Doctors_SpecializationId",
---
+++ InnoClinic.Profiles.Infrastructure/Migrations/20260629124811_AddSpecializationsTableInProfiles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoClinic.Profiles.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSpecializationsTableInProfiles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
