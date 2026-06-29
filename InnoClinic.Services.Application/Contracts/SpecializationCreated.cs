dbc43ab1c42dbd7bf098c1d4738c69c7b5c0561e
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-29 16:29:51 +0300

    add(prof.appl and serv.appl): add event SpecializationCreated

---
+++ InnoClinic.Services.Application/Contracts/SpecializationCreated.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Services.Application.Contracts
+{
+    public record SpecializationCreated(Guid Id, string SpecializationName);
+}
