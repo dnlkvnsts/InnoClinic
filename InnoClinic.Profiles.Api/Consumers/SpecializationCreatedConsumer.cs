4a9a269037a6be1c9c793e1a838a2a99dc7d98b3
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-29 18:06:32 +0300

    fix(prof.api): change program

--- InnoClinic.Profiles.Api/Consumers/SpecializationCreatedConsumer.cs
+++ InnoClinic.Profiles.Api/Consumers/SpecializationCreatedConsumer.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Logging;
 
 
 
+namespace InnoClinic.Profiles.Application.Consumers
+
 namespace InnoClinic.Profiles.Infrastructure.Consumers
 {
     public  class SpecializationCreatedConsumer : IConsumer<SpecializationCreated>
