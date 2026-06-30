90a79d16d846e8149943a819314a2a74addcc945
danilkova_anastasiya <danilkova905@gmail.com>
2026-06-30 15:41:04 +0300

    fix(prof.appl): change GetDoctorsQueryHandler

--- InnoClinic.Profiles.Application/Features/Doctors/Queries/GetDoctors/GetDoctorsQueryHandler.cs
+++ InnoClinic.Profiles.Application/Features/Doctors/Queries/GetDoctors/GetDoctorsQueryHandler.cs
@@ -23,14 +23,13 @@ namespace InnoClinic.Profiles.Application.Features.Doctors.Queries.GetDoctors
 
         public async Task<IEnumerable<DoctorDto>> Handle(GetDoctorsQuery request, CancellationToken cancellationToken)
         {
-            var query = _doctorRepository.GetDoctorsQuery();
+            var doctors = await _doctorRepository.GetDoctorsAsync(request.FullName, request.SpecializationId, cancellationToken);
 
-            query = query.Where(d => d.Status == "At work");
 
             var currentYear = System.DateTime.UtcNow.Year;
 
 
-            var result = await query.Select(d => new DoctorDto(
+            var result = doctors.Select(d => new DoctorDto(
                     d.PhotoUrl,
                     d.FirstName,
                     d.LastName,
@@ -39,11 +38,10 @@ namespace InnoClinic.Profiles.Application.Features.Doctors.Queries.GetDoctors
                     d.Specialization.SpecializationName,
                     currentYear - d.CareerStartYear + 1,
                     d.OfficeAddress
-                )).ToListAsync(cancellationToken);
+                )).ToList();
 
             return result;
         }
-
 
 
     }
