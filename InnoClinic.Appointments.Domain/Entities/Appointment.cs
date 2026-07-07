690bb20a7fe0515db91b4d045024b3994fb31486
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-07 16:38:39 +0300

    feat(appoint.dom): add entity Appointment

---
+++ InnoClinic.Appointments.Domain/Entities/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Appointments.Domain.Entities
+{
+   public  class Appointment
+   {
+        public Guid Id { get; set; }
+        public Guid PatientId { get; set; }
+        public Guid DoctorId { get; set; }
+        public Guid ServiceId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+        public bool IsApproved { get; set; } = false;
+    
+   }
+}
