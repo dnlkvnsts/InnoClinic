44bd80ee4e0b253571e63ecff26078ed8500cca4
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-07 17:27:42 +0300

    feat(appoint.appl): add CreateAppointmentCommandHandler

--- InnoClinic.Appointments.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
+++ InnoClinic.Appointments.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace InnoClinic.Appointments.Application.Features.Appointments.Commands.CreateAppointment
 {
-    internal class CreateAppointmentCommand
-    {
-    }
+    public record CreateAppointmentCommand(
+        Guid PatientId,
+        Guid DoctorId,
+        Guid ServiceId,
+        DateTime Date,
+        TimeSpan Time
+    ) : IRequest<Guid>;
 }
--- InnoClinic.Appointments.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
+++ InnoClinic.Appointments.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InnoClinic.Appointments.Application.Interfaces;
+using InnoClinic.Appointments.Domain.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,34 @@ using System.Threading.Tasks;
 
 namespace InnoClinic.Appointments.Application.Features.Appointments.Commands.CreateAppointment
 {
-    internal class CreateAppointmentCommandHandler
+    public  class CreateAppointmentCommandHandler : IRequestHandler<CreateAppointmentCommand, Guid>
     {
+
+        private readonly IAppointmentRepository _repository;
+
+        public CreateAppointmentCommandHandler(IAppointmentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Guid> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
+        {
+            var appointment = new Appointment
+            {
+                Id = Guid.NewGuid(),
+                PatientId = request.PatientId,
+                DoctorId = request.DoctorId,
+                ServiceId = request.ServiceId,
+                Date = request.Date.Date,
+                Time = request.Time,
+                IsApproved = false
+            };
+
+            await _repository.AddAsync(appointment, cancellationToken);
+            await _repository.SaveChangesAsync(cancellationToken);
+
+            return appointment.Id;
+        }
+
     }
 }
