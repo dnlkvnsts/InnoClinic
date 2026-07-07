e26cd8e8dd5064e78b935943b5cbdd5d0e350320
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-07 17:15:26 +0300

    feat(appoint.appl): add AppointmentDto and IAppointmentRepository

---
+++ InnoClinic.Appointments.Application/Features/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Appointments.Application.Features.Appointments.Commands.CreateAppointment
+{
+    internal class CreateAppointmentCommandHandler
+    {
+    }
+}
