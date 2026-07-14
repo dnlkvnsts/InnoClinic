c112f22c037f9d761ca192fded02bafda3d8ed93
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-14 10:33:30 +0300

    feat(app.infra): add ServiceCreatedConsumer

---
+++ InnoClinic.Appointments.Infrastructure/Consumers/ServiceCreatedConsumer.cs
@@ -0,0 +1,48 @@
+﻿using InnoClinic.Appointments.Domain;
+using InnoClinic.Appointments.Infrastructure.Persistence;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Appointments.Infrastructure.Consumers
+{
+    public class ServiceCreatedConsumer : IConsumer<ServiceCreated>
+    {
+        private readonly AppointmentsDbContext _context;
+
+
+        public ServiceCreatedConsumer(AppointmentsDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+        public async Task Consume(ConsumeContext<ServiceCreated> context)
+        {
+            var message = context.Message;
+
+            var existingService = await _context.Services.FindAsync(message.Id);
+            if (existingService != null) return;
+
+
+            var service = new Service
+            {
+                Id = message.Id,
+                ServiceName = message.ServiceName,
+                Price = message.Price
+            };
+
+
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+        }
+
+
+
+    }
+}
