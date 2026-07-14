3777d4bb526420eee908f61f29bc6024d4912229
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-14 10:05:02 +0300

    feat(serv.api): add settings to program

--- InnoClinic.Services.Api/Program.cs
+++ InnoClinic.Services.Api/Program.cs
@@ -55,6 +55,22 @@ builder.Services.AddMassTransit(x =>
 
 
 
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+    });
+});
+
+
+
+
 
 
 var app = builder.Build();
