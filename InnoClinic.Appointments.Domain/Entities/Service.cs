f0e15f198ad06d899d9196001583527d7092f98a
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-14 10:28:28 +0300

    feat(app.dom): add Service

---
+++ InnoClinic.Appointments.Domain/Entities/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Appointments.Domain.Entities
+{
+    public  class Service
+    {
+
+        public Guid Id { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+
+        public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    }
+}
