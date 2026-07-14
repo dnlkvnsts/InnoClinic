7da0ef6eb01510dfcc1842eddda849a1b54fc62f
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-14 15:16:51 +0300

    feat(prof.appl): add GetPatientsQueryHandler

---
+++ InnoClinic.Profiles.Application/Features/Patients/Queries/GetPatients/GetPatientsQuery.cs
@@ -0,0 +1,12 @@
+﻿using InnoClinic.Profiles.Application.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Profiles.Application.Features.Patients.Queries.GetPatients
+{
+    public record GetPatientsQuery() : IRequest<IEnumerable<PatientDto>>;
+}
---
+++ InnoClinic.Profiles.Application/Features/Patients/Queries/GetPatients/GetPatientsQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using InnoClinic.Appointments.Domain;
+using InnoClinic.Profiles.Application.DTOs;
+using InnoClinic.Profiles.Application.Features.Doctors.Queries.GetDoctors;
+using InnoClinic.Profiles.Application.Interfaces;
+using MassTransit;
+using MediatR;
+
+
+namespace InnoClinic.Profiles.Application.Features.Patients.Queries.GetPatients
+{
+    public class GetPatientsQueryHandler : IRequestHandler<GetPatientsQuery, IEnumerable<PatientDto>>
+    {
+
+        private readonly IPatientRepository _patientRepository;
+     
+
+
+        public GetPatientsQueryHandler(IPatientRepository patientRepository)
+        {
+           _patientRepository = patientRepository;
+        }
+
+
+        public async Task<IEnumerable<PatientDto>> Handle(GetPatientsQuery request, CancellationToken cancellationToken)
+        {
+            var patients = _patientRepository.GetPatientsQuery().ToList();
+
+
+
+            var result = patients.Select(p => new PatientDto(
+                p.FirstName,
+                p.LastName,
+                p.MiddleName,
+                p.DateOfBirth,
+                p.IsLinkedToAccount
+                )).ToList();
+
+            return result;
+        }
+
+
+
+
+    }
+}
