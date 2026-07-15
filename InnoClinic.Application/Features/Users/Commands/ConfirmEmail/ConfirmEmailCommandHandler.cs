4fc5cf4e9ac0edf7f593a02e9d02d63c1ba099e5
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-15 15:29:15 +0300

    feat(auth.appl): add ConfirmEmailCommandHandler

---
+++ InnoClinic.Application/Features/Users/Commands/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using InnoClinic.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Auth.Application.Features.Users.Commands.ConfirmEmail
+{
+    public class ConfirmEmailCommandHandler : IRequestHandler<ConfirmEmailCommand, ConfirmEmailResponse>
+    {
+        private readonly IIdentityService _identityService;
+
+        public ConfirmEmailCommandHandler(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public async Task<ConfirmEmailResponse> Handle(ConfirmEmailCommand request, CancellationToken cancellationToken)
+        {
+            var (isSuccess, errors) = await _identityService.ConfirmEmailAsync(request.UserId, request.Token);
+
+            return new ConfirmEmailResponse(isSuccess, errors);
+        }
+    }
+}
