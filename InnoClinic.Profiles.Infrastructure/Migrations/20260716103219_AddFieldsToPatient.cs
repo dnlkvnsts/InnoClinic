d4608faad27666dd52f8d83b792bb9621ca03ed5
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-16 13:34:31 +0300

    feat(prof.appl): add data to PatientDto

---
+++ InnoClinic.Profiles.Infrastructure/Migrations/20260716103219_AddFieldsToPatient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InnoClinic.Profiles.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsToPatient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                columns: new[] { "Phone", "PhotoUrl" },
+                values: new object[] { "+1234567890", "https://example.com/photos/emilybrown.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"),
+                columns: new[] { "Phone", "PhotoUrl" },
+                values: new object[] { "+10987654321", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Patients");
+        }
+    }
+}
