4d9a5fede6286d3844e1b88ee2b075c7864afe08
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-16 13:52:07 +0300

    feat(prof.appl): add CreatePatientCommandHandler

---
+++ InnoClinic.Profiles.Application/Features/Patients/Commands/CreatePatient/CreatePatientCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoClinic.Profiles.Application.Features.Patients.Commands.CreatePatient
+{
+    public record CreatePatientCommand(
+        string FirstName,
+        string LastName,
+        string? MiddleName,
+        string Phone,
+        DateTime DateOfBirth,
+        string? PhotoUrl,
+        Guid AccountId
+    ) : IRequest<Guid>;
+}
