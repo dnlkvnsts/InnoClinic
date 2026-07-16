026e40ac60c9f0a0501af226466c1ef8ab4bc75a
danilkova_anastasiya <danilkova905@gmail.com>
2026-07-16 14:02:21 +0300

    feat(prof.api):add endpoint to PatientController

--- InnoClinic.Profiles.Api/Controllers/PatientController.cs
+++ InnoClinic.Profiles.Api/Controllers/PatientController.cs
@@ -1,4 +1,5 @@
 ﻿using InnoClinic.Profiles.Application.Features.Doctors.Queries.GetDoctors;
+using InnoClinic.Profiles.Application.Features.Patients.Commands.CreatePatient;
 using InnoClinic.Profiles.Application.Features.Patients.Queries.GetPatients;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,17 @@ namespace InnoClinic.Profiles.Api.Controllers
 
             return Ok(patients);
         }
+
+
+
+        [HttpPost("createpatient")]
+        public async Task<IActionResult> CreatePatient([FromBody] CreatePatientCommand request, CancellationToken cancellationToken)
+        {
+            Guid patientId = await _mediator.Send(request, cancellationToken);
+            return Ok(new { Id = patientId });
+
+        }
+
 
 
     }
