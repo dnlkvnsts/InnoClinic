856ace8dbc70c9b604668faa9bc4e70410cd4bc8
Nastya <danilkova905@gmail.com>
2026-08-03 15:32:41 +0300

    Add CreateSpecializationCommandHandler (#6)
    * feat(prof.domain): add specialization to entities of profiles
    
    * faet(prof.infra): add new doctor table in db
    
    * faet(prof.infra): add specialization to GetDoctorsQuery
    
    * fix(prof.appl): change DoctorDto
    
    * fix(prof.appl): change GetDoctorsQueryHandler
    
    * add(prof.appl and serv.appl): add event SpecializationCreated
    
    * add(serv.api): add MassTransit settings to program
    
    * add(serv.api): implement CreateSpecialization
    
    * feat(prof.api): add and register consumer
    
    * fix(prof.api): change program
    
    * fix(prof.api): change the version of MassTransit
    
    * feat(shared): add new library Shared
    
    * fix: add some changes
    
    * feat(prof.infrra): delete unnecessary migration
    
    * feat(prof.infrra): change namespace in SpecializationCreatedConsumer
    
    * fix(prof.infra): change the field on nullable : true
    
    * feat(prof.infra): add checking about existence of specialization

--- InnoClinic.Profiles.Api/Program.cs
+++ InnoClinic.Profiles.Api/Program.cs
@@ -1,9 +1,13 @@
+using InnoClinic.Profiles.Infrastructure.Consumers;
+using InnoClinic.Shared.Contracts;
 using InnoClinic.Profiles.Application.Features.Doctors.Queries.GetDoctors;
 using InnoClinic.Profiles.Application.Interfaces;
 using InnoClinic.Profiles.Infrastructure.Persistence;
 using InnoClinic.Profiles.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
+using MassTransit;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +37,29 @@ builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(GetDoctorsQuery).Assembly);
 
 });
+
+
+
+builder.Services.AddMassTransit(x =>
+{
+
+    x.AddConsumer<SpecializationCreatedConsumer>();
+
+    x.UsingRabbitMq((context, sfg) =>
+    {
+        sfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        sfg.ConfigureEndpoints(context);
+    });
+
+});
+
+
+
 
 var app = builder.Build();
 
---
+++ InnoClinic.Profiles.Infrastructure/Consumers/SpecializationCreatedConsumer.cs
@@ -0,0 +1,50 @@
+﻿using InnoClinic.Profiles.Domain.Entities;
+using InnoClinic.Profiles.Infrastructure.Persistence;
+using InnoClinic.Shared.Contracts;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace InnoClinic.Profiles.Infrastructure.Consumers
+{
+    public  class SpecializationCreatedConsumer : IConsumer<SpecializationCreated>
+    {
+        private readonly ProfilesDbContext _context;
+        private readonly ILogger<SpecializationCreatedConsumer> _logger;
+
+        public SpecializationCreatedConsumer(ProfilesDbContext context, ILogger<SpecializationCreatedConsumer> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<SpecializationCreated> context)
+        {
+            var message = context.Message;
+
+            var exists = await _context.Specializations
+                .AnyAsync(s => s.Id == message.Id, context.CancellationToken);
+
+
+            if (exists)
+            {
+                _logger.LogWarning("Specialization with Id {SpecializationId} already exists. Skipping duplicate message.", message.Id);
+                return; 
+            }
+
+            var localSpecialization = new Specialization
+            {
+                Id = message.Id,                       
+                SpecializationName = message.SpecializationName,
+                IsActive = true
+            };
+
+           
+            _context.Specializations.Add(localSpecialization);
+            await _context.SaveChangesAsync(context.CancellationToken);
+        }
+
+    }
+}
